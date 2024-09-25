476b5698c75fc3e4306b31c232d6ea5bea7b174e
Lloyd Vheremu <lloydjunior1@gmail.com>
2024-09-26 01:51:27 +0200

    Created a header file for the Shaderr Program

--- HelloWorld/shaders_interpolation.cpp
+++ HelloWorld/shaders_interpolation.cpp
@@ -1,29 +1,9 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
+#include "Shader.h"
 
-// Shader sources
-const char* vertexShaderSource = R"(
-    #version 330 core
-    layout (location = 0) in vec3 aPos; // position has attribute position 0
-    layout (location = 1) in vec3 aColor; // color has attribute position 1
 
-    out vec3 ourColor; // specify a color output to the fragment shader
-    void main() {
-        gl_Position = vec4(aPos, 1.0);
-        ourColor = aColor;
-    }
-)";
-
-const char* fragmentShaderSource = R"(
-    #version 330 core
-    out vec4 FragColor;    
-    in vec3 ourColor; 
-    void main() {
-        FragColor = vec4(ourColor, 1.0);
-   
-    }
-)";
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
@@ -42,7 +22,7 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Create window
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Learn OpenGL", NULL, NULL);
     if (!window) {
         std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
@@ -92,28 +72,7 @@ int main() {
 
 
     // Create shaders
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-
-    // Create shader program
-    unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-
-    // Check linking errors
-    int success;
-    char infoLog[512];
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cerr << "ERROR::SHADER::Program::LINK_FAILED\n" << infoLog << std::endl;
-    }
+    Shader ourShader("./shader.vs", "./shader.fs");
 
     // Check maximum input variables or vertex attributes allowed by current hardware
     int nrAttributes;
@@ -128,13 +87,14 @@ int main() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shaderProgram);
+        ourShader.use();
+        ourShader.setFloat("someUniform", 1.0f);
 
-        // update the uniform color variable;
-        float timeValue = glfwGetTime();
-        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+        //// update the uniform color variable;
+        //float timeValue = glfwGetTime();
+        //float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+        //int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+        //glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
         // rebind VAO and render the triangle
         glBindVertexArray(VAO);
@@ -146,9 +106,6 @@ int main() {
     }
 
     // Cleanup
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-    glDeleteProgram(shaderProgram);
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glfwTerminate();
